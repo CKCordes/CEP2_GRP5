3bb30ea8354670016399f0e11e3377941900f57d
mathiesschou@icloud.com <mathiesschou@icloud.com>
2024-04-04 10:51:50 +0200

    Roomtracker iteration 1

--- app/Main.py
+++ app/Main.py
@@ -8,12 +8,6 @@ if __name__ == "__main__":
 
     stove_devices_model = DevicesModel()
     
-    stove_devices_model.add_devices([ZigbeeDevice("test1", "pir", "kitchen"),
-                                     ZigbeeDevice("test2", "pir", "bedroom"),
-                                     ZigbeeDevice("test3", "pir", "bathroom"),
-                                     ZigbeeDevice("test4", "pir", "living room"),
-                                     ZigbeeDevice("test5", "pir", "hall")])
-
     stove_devices_model.add_devices([ZigbeeDevice("test", "power plug", "kitchen")])
 
     stove_tracker = KitchenStoveTracker("StoveTracker", stove_devices_model)
