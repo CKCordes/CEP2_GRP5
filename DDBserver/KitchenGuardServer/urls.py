0d925e511443abef82efd7cd26f63c5a28eda079
Mads Kolind <Madsskolind@gmail.com>
2024-04-18 12:08:45 +0200

    Simple html, css and js implemented

--- DDBserver/KitchenGuardServer/urls.py
+++ DDBserver/KitchenGuardServer/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.login, name="login"),
     path("index", views.index, name="index"),
     path("KitchGuardServer", views.index, name="index"),
-    path("dbupdater", views.dbupdater, name="dbupdater")
+    path("dbupdater", views.dbupdater, name="dbupdater"),
+    path("allEvents", views.allEvents, name="allEvents"),
+    path("someEvents", views.someEvents, name="someEvents"),
 ]
--- DDBserver/KitchenGuardServer/views.py
+++ DDBserver/KitchenGuardServer/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Patient
+from django.http import HttpResponse, JsonResponse
+from .models import Patient, Sensor, Event
 from .KGscripts.update_db import updatedb
 import json
 from django.views.decorators.csrf import csrf_exempt
+
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
@@ -40,3 +41,28 @@ def dbupdater(request):
         return HttpResponse("POST success")
     except:    
         return HttpResponse("Her skal du POST")
+    
+def allEvents(request):
+    try:
+        data = Event.objects.all()
+        
+        serialized_data = [{'event_id': item.event_id, 
+                            'sensor': item.sensor.sensor_id,
+                            'patient': item.patient.patient_id} for item in data]
+        return JsonResponse({'data': serialized_data})    
+        
+    except:
+        return HttpResponse("SOMETHING'S BAD")
+    
+def someEvents(request):
+    try:
+        data = Event.objects.all()
+        
+        serialized_data = [{'event_id': item.event_id, 
+                            'sensor': item.sensor.sensor_id,
+                            'patient': item.patient.patient_id,
+                            'event_type': item.event_type} for item in data]
+        return JsonResponse({'data': serialized_data})    
+        
+    except:
+        return HttpResponse("SOMETHING'S BAD")
